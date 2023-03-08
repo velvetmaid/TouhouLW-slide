c436a44e6b8b01e42e790666d55dbfbc256e12f7
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-03-08 13:04:23 +0700

    build: setup {boilerplate, tailwind, swiper}

---
+++ src/js/swiper.js
@@ -0,0 +1,6 @@
+ // import Swiper JS
+ import Swiper from 'swiper';
+ // import Swiper styles
+ import 'swiper/css';
+
+ const swiper = new Swiper(...);
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}'], // add your preferred file extensions here
+  theme: {
+    extend: {
+      backgroundImage: {
+        'background': "url('../img/bg.jpg')",
+      }
+    },
+  },
+  plugins: [],
+};
